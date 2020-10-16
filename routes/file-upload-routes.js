28d7fd9c47c7bd2db2468a19d29f706387d5f84a
Arua Monteiro <aruamonteiro@gmail.com>
2020-10-16 09:14:48 -0300

    adjust upload route

--- routes/file-upload-routes.js
+++ routes/file-upload-routes.js
@@ -1,25 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 // include CLOUDINARY:
 
-const uploader = require('../configs/cloudinary.config.js');
+const uploader = require("../configs/cloudinary.config.js");
 
-router.post('/upload', uploader.single('imageUrl'), (req, res, next) => {
+router.post("/upload", uploader.single("attachment"), (req, res, next) => {
   // console.log('file is: ', req.file)
 
   if (!req.file) {
-    next(new Error('No file uploaded!'));
+    next(new Error("No file uploaded!"));
 
     return;
   }
 
-  // get secure_url from the file object and save it in the
-
-  // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
-
-  res.json({ secure_url: req.file.secure_url });
+  res.json({ image: req.file.secure_url });
 });
 
 module.exports = router;
