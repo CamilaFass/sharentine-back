3b9a929141bce502dd34c6a0764f6be5dfbc7f25
Camila Fassarella <fassarella.camila@gmail.com>
2020-10-18 20:21:29 -0300

    v1.0

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 // Configura o app para entender requisições com tipo de corpo JSON
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: process.env.CORS }));
 
-const authRouter = require('./routes/auth.routes');
-const postRouter = require('./routes/post.routes');
-const commentRouter = require('./routes/comment.routes');
+const authRouter = require("./routes/auth.routes");
+const postRouter = require("./routes/post.routes");
+const commentRouter = require("./routes/comment.routes");
+const path = require("path");
 
-require('./configs/db.config');
+require("./configs/db.config");
 
-require('./configs/passport.config')(app);
+require("./configs/passport.config")(app);
 
-app.use('/api', authRouter);
-app.use('/api', postRouter);
-app.use('/api', commentRouter);
+app.use("/api", authRouter);
+app.use("/api", postRouter);
+app.use("/api", commentRouter);
 
-app.use('/api', require('./routes/file-upload-routes'));
+app.use("/api", require("./routes/file-upload-routes"));
 
-app.listen(5000, () => console.log('running at port 5000'));
+app.listen(process.env.PORT, () =>
+  console.log(`running at port ${process.env.PORT}`)
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.join(__dirname, "/public")));
+app.use((req, res, next) => {
+  const hostUrl = req.get("host");
+  if (hostUrl.includes("/api") === true) {
+    return res.sendFile(__dirname + "/public/index.html");
+  }
+  return;
+});
